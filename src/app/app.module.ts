9d13f1b332180f05e85291b1ed3323bb4803aa59
sakshidheer <69763547+sakshidheer@users.noreply.github.com>
2022-10-30 23:26:15 +0530

    Updating to use vanilla html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 
 
@@ -22,10 +18,6 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
